07082f56bd88ddc55c821e9de4286ec2a772be38
Shanyik <tkrisi213@gmail.com>
2024-01-17 10:15:50 +0100

    succesfully connected frontend and backend

--- nginx-frontend/src/App.js
+++ nginx-frontend/src/App.js
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [test, setTest] = useState(null)
+
+  const handleOnClick = () => {
+    fetch('http://localhost:5136/api/Test/test', {
+      method: 'GET',
+  })
+      .then(response => {return response.text()})
+      .then(data => {
+          setTest(data)
+          console.log(data);
+      }
+      )
+      .catch(error => console.log(error))
+  }
+
   return (
+    
     <div className="App">
-      cleaned frontend
+      <button onClick={() => {handleOnClick()}}>Test Connection</button>
+      {
+        test ? (
+          <p>{test}</p>
+        ) : (
+          <p>Waiting for connection...</p>
+        )
+      }
     </div>
+    
   );
 }
 
